02de22ca69aa2e373e5d4b804b5ea088819b55ba
FJPdeJong <frank.049@hotmail.com>
2016-11-23 15:54:04 +0100

    Changed package to accomodate project structure in intelliJ

--- src/taxi/TaxiScheduling.java
+++ src/taxi/TaxiScheduling.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package taxi;
+package src.taxi;
 
 import java.util.LinkedList;
 import java.util.Queue;
-// test op
+// test op ja
 
 public class TaxiScheduling {
     int l;
